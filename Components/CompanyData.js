0f25b54a5852d210577cbc83c70be770d3b4b826
Deepak Meena <deepak07itmeena@gmail.com>
2024-05-25 15:43:04 +0530

    components

---
+++ Components/CompanyData.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const CompanyData = () => {
+  return (
+    <View>
+      <Text style={{fontSize: 20}}>Name</Text>
+      <Text style={{fontSize: 20}}>Name</Text>
+    </View>
+  );
+};
+
+export default CompanyData;
