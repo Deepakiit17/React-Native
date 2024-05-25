37db67612949211ae1d9ec3afcaffac33a46436b
Deepak Meena <deepak07itmeena@gmail.com>
2024-05-25 16:02:47 +0530

    state

--- App.tsx
+++ App.tsx
@@ -1,21 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Text, View} from 'react-native';
 
 function App() {
-  let data = 100;
-  const fruit = val => {
-    data = 20;
-    console.warn(data);
+  const [name, setName] = useState('anil');
+  let data = 'Sam';
+  const fruit = () => {
+    setName('Sidhu');
+    data = 'peter';
   };
   return (
     <View>
+      <Text style={{fontSize: 40}}>{name}</Text>
       <Text style={{fontSize: 40}}>{data}</Text>
-      <Button
-        title="Press Here"
-        onPress={() => fruit('Hello Anil')}
-        color={'red'}
-      />
-      <Button title="Press Here 2" onPress={fruit} color={'green'} />
+      <Button title="Update Name" onPress={fruit} color={'red'} />
     </View>
   );
 }
