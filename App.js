578af39a3b689ebf99fdc7034af59c0dacebd7f2
Deepak Meena <deepak07itmeena@gmail.com>
2024-05-25 22:04:02 +0530

    component with list

--- App.js
+++ App.js
@@ -1,108 +1,35 @@
 import React, {useState} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {FlatList, Text, View} from 'react-native';
+import UserData from './Components/CompanyData';
 
 function App() {
   const users = [
     {
       id: 1,
       name: 'anil',
+      email: 'anil@text.com',
     },
     {
       id: 2,
       name: 'sam',
+      email: 'sam@text.com',
     },
     {
       id: 3,
       name: 'peter',
-    },
-    {
-      id: 4,
-      name: 'bruce',
-    },
-    {
-      id: 5,
-      name: 'bruce',
-    },
-    {
-      id: 4,
-      name: 'bruce',
-    },
-    {
-      id: 4,
-      name: 'bruce',
-    },
-    {
-      id: 4,
-      name: 'bruce',
-    },
-    {
-      id: 4,
-      name: 'bruce',
-    },
-    {
-      id: 4,
-      name: 'bruce',
-    },
-    {
-      id: 4,
-      name: 'bruce',
-    },
-    {
-      id: 4,
-      name: 'bruce',
-    },
-    {
-      id: 10,
-      name: 'tony',
-    },
-    {
-      id: 4,
-      name: 'bruce',
-    },
-    {
-      id: 4,
-      name: 'bruce',
-    },
-    {
-      id: 4,
-      name: 'bruce',
-    },
-    {
-      id: 4,
-      name: 'bruce',
-    },
-    {
-      id: 4,
-      name: 'bruce',
+      email: 'pet@text.com',
     },
   ];
 
   return (
     <View>
       <Text style={{fontSize: 40}}>Grid with Dynamic Data in RN</Text>
-      <View style={{flex: 1, flexDirection: 'row', flexWrap: 'wrap'}}>
-        {users.map(item => (
-          <Text style={styles.textInput}>{item.name}</Text>
-        ))}
-      </View>
+      <FlatList
+        data={users}
+        renderItem={({item}) => <UserData item={item} />}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  textInput: {
-    fontSize: 25,
-    color: '#fff',
-    padding: 5,
-    backgroundColor: 'blue',
-    borderWidth: 1,
-    borderColor: 'black',
-    margin: 5,
-    width: 120,
-    height: 120,
-    textAlignVertical: 'center',
-    textAlign: 'center',
-  },
-});
 
 export default App;
--- Components/CompanyData.js
+++ Components/CompanyData.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 
-const CompanyData = () => {
+export default UserData = props => {
+  const item = props.item;
   return (
-    <View>
-      <Text style={{fontSize: 20}}>Name</Text>
-      <Text style={{fontSize: 20}}>Name</Text>
+    <View style={styles.box}>
+      <Text style={styles.textInput}>{item.name}</Text>
+      <Text style={styles.textInput}>{item.email}</Text>
     </View>
   );
 };
 
-export default CompanyData;
+const styles = StyleSheet.create({
+  textInput: {
+    fontSize: 24,
+    color: 'orange',
+    flex: 1,
+    margin: 2,
+    textAlign: 'center',
+  },
+  box: {
+    flexDirection: 'row',
+    borderWidth: 2,
+    borderColor: 'orange',
+    marginBottom: 10,
+  },
+});
