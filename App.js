a34b2b0fff1fa5015825bb3c481bc8f4160f5508
Deepak Meena <deepak07itmeena@gmail.com>
2024-05-27 08:06:05 +0530

    Hide Show

--- App.js
+++ App.js
@@ -2,45 +2,21 @@ import React, {useEffect, useState} from 'react';
 import {Button, Text, View} from 'react-native';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [data, setData] = useState(100);
-
-  // useEffect(() => {
-  //   console.warn('do some animation');
-  // }, [count]);
-
-  // useEffect(() => {
-  //   console.warn('call some api here');
-  // }, [data]);
+  const [show, setShow] = useState(false);
 
   return (
     <View>
       <Text style={{fontSize: 40}}>useEffect as componentDidUpdate in RN</Text>
-      <Text style={{fontSize: 30}}>{count}</Text>
-      <Text style={{fontSize: 40}}>{data}</Text>
-      <Button title="Update Count" onPress={() => setCount(count + 1)} />
-      <Button title="Update Data" onPress={() => setData(data + 1)} />
-      <User info={{data, count}} />
+      <Button title="Toggle Component" onPress={() => setShow(!show)} />
+      {show ? <User /> : null}
     </View>
   );
 }
 
 const User = props => {
-  useEffect(() => {
-    console.warn('run this code when count prop is updated');
-  }, [props.info.count]);
-
-  useEffect(() => {
-    console.warn('run this code when data prop is updated');
-  }, [props.info.data]);
   return (
     <View>
-      <Text style={{fontSize: 30, color: 'orange'}}>
-        Data : {props.info.count}
-      </Text>
-      <Text style={{fontSize: 30, color: 'orange'}}>
-        Data : {props.info.data}
-      </Text>
+      <Text style={{fontSize: 30, color: 'orange'}}>User Component</Text>
     </View>
   );
 };
