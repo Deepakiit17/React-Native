ba19fd26fc92a369e4ec4ff5d58f8772a2797032
Deepak Meena <deepak07itmeena@gmail.com>
2024-05-27 16:19:58 +0530

    dynamic radio css

--- App.js
+++ App.js
@@ -2,26 +2,43 @@ import React, {useState} from 'react';
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
 function App() {
+  const skills = [
+    {
+      id: 1,
+      name: 'JAVA',
+    },
+    {
+      id: 2,
+      name: 'PHP',
+    },
+    {
+      id: 3,
+      name: 'Node',
+    },
+    {
+      id: 4,
+      name: 'SQL',
+    },
+    {
+      id: 5,
+      name: 'Python',
+    },
+  ];
   const [selectedRadio, setSelectedRadio] = useState(2);
   return (
     <View style={styles.main}>
-      <TouchableOpacity onPress={() => setSelectedRadio(1)}>
-        <View style={styles.radioWrapper}>
-          <View style={styles.radio}>
-            {selectedRadio === 1 ? <View style={styles.radioBg}></View> : null}
+      {skills.map((item, index) => (
+        <TouchableOpacity key={index} onPress={() => setSelectedRadio(item.id)}>
+          <View style={styles.radioWrapper}>
+            <View style={styles.radio}>
+              {selectedRadio === item.id ? (
+                <View style={styles.radioBg}></View>
+              ) : null}
+            </View>
+            <Text style={styles.radioText}>{item.name}</Text>
           </View>
-          <Text style={styles.radioText}>Radio 1</Text>
-        </View>
-      </TouchableOpacity>
-
-      <TouchableOpacity onPress={() => setSelectedRadio(2)}>
-        <View style={styles.radioWrapper}>
-          <View style={styles.radio}>
-            {selectedRadio === 2 ? <View style={styles.radioBg}></View> : null}
-          </View>
-          <Text style={styles.radioText}>Radio 2</Text>
-        </View>
-      </TouchableOpacity>
+        </TouchableOpacity>
+      ))}
     </View>
   );
 }
