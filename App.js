3d396d67048d85927979e0176416187987f09521
Deepak Meena <deepak07itmeena@gmail.com>
2024-05-27 14:28:07 +0530

    button css

--- App.js
+++ App.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, Text, TouchableHighlight, View} from 'react-native';
 
 function App() {
   return (
     <View style={styles.main}>
-      <View style={styles.box1}>
-        <View style={styles.innerBox1}></View>
-        <View style={styles.innerBox2}></View>
-        <View style={styles.innerBox3}></View>
-      </View>
-      <View style={styles.box2}></View>
-      <View style={styles.box3}></View>
+      <TouchableHighlight>
+        <Text style={[styles.button, styles.success]}>Success</Text>
+      </TouchableHighlight>
+      <TouchableHighlight>
+        <Text style={[styles.button, styles.primary]}>Primary</Text>
+      </TouchableHighlight>
+      <TouchableHighlight>
+        <Text style={[styles.button, styles.warning]}>Warning</Text>
+      </TouchableHighlight>
+      <TouchableHighlight>
+        <Text style={[styles.button, styles.error]}>Error</Text>
+      </TouchableHighlight>
+      <TouchableHighlight>
+        <Text style={styles.button}>Button</Text>
+      </TouchableHighlight>
     </View>
   );
 }
@@ -19,34 +27,22 @@ const styles = StyleSheet.create({
   main: {
     flex: 1,
   },
-  box1: {
-    flex: 2,
-    backgroundColor: 'red',
-    flexDirection: 'row',
-  },
-  box2: {
-    flex: 1,
-    backgroundColor: 'green',
-  },
-  box3: {
-    flex: 1,
-    backgroundColor: 'blue',
-  },
-  innerBox1: {
-    flex: 1,
-    backgroundColor: 'skyblue',
+  button: {
+    backgroundColor: '#bbb',
+    color: '#fff',
+    fontSize: 24,
+    textAlign: 'center',
+    padding: 10,
     margin: 10,
+    borderRadius: 10,
+    shadowColor: 'black',
+    elevation: 10,
+    shadowOpacity: 1,
   },
-  innerBox2: {
-    flex: 1,
-    backgroundColor: 'blue',
-    margin: 10,
-  },
-  innerBox3: {
-    flex: 1,
-    backgroundColor: 'orange',
-    margin: 10,
-  },
+  success: {backgroundColor: 'green'},
+  primary: {backgroundColor: 'blue'},
+  warning: {backgroundColor: 'gold'},
+  error: {backgroundColor: 'red'},
 });
 
 export default App;
