72bbe8104e494bf9edd162c4d2abd80491795a3d
Deepak Meena <deepak07itmeena@gmail.com>
2024-05-27 06:35:00 +0530

    useEffect

--- App.js
+++ App.js
@@ -1,34 +1,20 @@
-import React, {Component} from 'react';
-import {Button, Text, TextInput, View} from 'react-native';
-import Student from './Components/CompanyData';
+import React, {useEffect, useState} from 'react';
+import {Button, Text, View} from 'react-native';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'Anil',
-    };
-  }
+function App() {
+  const [count, setCount] = useState(0);
 
-  updateName(val) {
-    this.setState({name: val});
-  }
-  render() {
-    return (
-      <View>
-        <Text style={{fontSize: 40, color: 'blue'}}>
-          State & Props in Class Component in RN
-        </Text>
-        <Text style={{fontSize: 30, color: 'red'}}>{this.state.name}</Text>
-        <TextInput
-          placeholder="Enter Your Name"
-          onChangeText={text => this.updateName(text)}
-        />
-        <Button title="Press Me" />
-        <Student name={this.state.name} />
-      </View>
-    );
-  }
+  useEffect(() => {
+    console.warn('Hello');
+  }, []);
+
+  return (
+    <View>
+      <Text style={{fontSize: 40}}>Life Cycle with useEffect in RN</Text>
+      <Text style={{fontSize: 30}}>{count}</Text>
+      <Button title="Update Count" onPress={setCount(2)} />
+    </View>
+  );
 }
 
 export default App;
