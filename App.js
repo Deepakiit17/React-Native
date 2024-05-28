f063ac1c4ce9ccc00fecae4e9c152db6fed4f4a8
Deepak Meena <deepak07itmeena@gmail.com>
2024-05-28 22:33:20 +0530

    stack navigation style css

--- App.js
+++ App.js
@@ -7,8 +7,29 @@ const Stack = createNativeStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: 'blue',
+          },
+          headerTintColor: 'orange',
+          headerTitleStyle: {
+            fontSize: 25,
+          },
+        }}>
+        <Stack.Screen
+          options={{
+            headerStyle: {
+              backgroundColor: 'green',
+            },
+            headerTintColor: 'white',
+            headerTitleStyle: {
+              fontSize: 25,
+            },
+          }}
+          name="Login"
+          component={Login}
+        />
         <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
@@ -28,7 +49,7 @@ const Login = props => {
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text style={{fontSize: 30}}>Lock Screen</Text>
       <Button
-        title="Go to Home Page"
+        title="Go to Home Screen"
         onPress={() => props.navigation.navigate('Home')}
       />
     </View>
