fabcff122a1ec6777aa8c4ba023ed5e4d3503f6c
Deepak Meena <deepak07itmeena@gmail.com>
2024-05-29 11:46:31 +0530

    pass data between screen or components

--- Components/Login.js
+++ Components/Login.js
@@ -1,12 +1,20 @@
-import {Button, Text, View} from 'react-native';
+import {useState} from 'react';
+import {Button, Text, TextInput, View} from 'react-native';
 
 export const Login = props => {
+  const [name, setName] = useState('');
+  const age = 29;
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text style={{fontSize: 30}}>Lock Screen</Text>
+      <TextInput
+        style={{fontSize: 20, borderColor: '#000', borderWidth: 2}}
+        onChangeText={text => setName(text)}
+        placeholder="enter name"
+      />
       <Button
         title="Go to Home Screen"
-        onPress={() => props.navigation.navigate('Home')}
+        onPress={() => props.navigation.navigate('Home', {name, age})}
       />
     </View>
   );
