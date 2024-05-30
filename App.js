e9b6eaca3a6943c27f57e294347473059835a456
Deepak Meena <deepak07itmeena@gmail.com>
2024-05-30 16:19:03 +0530

    Simple POST API Call

--- App.js
+++ App.js
@@ -1,33 +1,27 @@
-import React, {useEffect, useState} from 'react';
-import {FlatList, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Button, FlatList, Text, View} from 'react-native';
 
 function App() {
-  const [data, setData] = useState([]);
-  const getAPIData = async () => {
-    const url = 'http://192.168.162.154:3000/users';
-    let result = await fetch(url);
+  const saveAPIData = async () => {
+    const data = {
+      name: 'Killer',
+      age: 36,
+      email: 'killer@test.com',
+    };
+    const url = 'http://10.0.2.2:3000/users';
+    let result = await fetch(url, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(data),
+    });
     result = await result.json();
-    setData(result);
+    console.warn(result);
   };
-
-  useEffect(() => {
-    getAPIData();
-  }, []);
 
   return (
     <View>
-      <Text style={{fontSize: 40}}>Call JSON server API Call</Text>
-      {data.length
-        ? data.map(item => (
-            <View style={{borderWidth: 1, borderColor: 'red'}}>
-              <Text style={{fontSize: 30, backgroundColor: 'aqua'}}>
-                id : {item.id}
-              </Text>
-              <Text style={{fontSize: 30}}>{item.name}</Text>
-              <Text style={{fontSize: 30}}>{item.age}</Text>
-            </View>
-          ))
-        : null}
+      <Text style={{fontSize: 40}}>POST API Call</Text>
+      <Button title="Save Data" onPress={saveAPIData} />
     </View>
   );
 }
