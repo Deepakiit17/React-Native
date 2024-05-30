5baf6910e8e95c67e95a4dfc1a0a2dc617a30ae3
Deepak Meena <deepak07itmeena@gmail.com>
2024-05-30 08:42:06 +0530

    FLatlist with  API call

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 import React, {useEffect, useState} from 'react';
-import {ScrollView, Text, View} from 'react-native';
+import {FlatList, Text, View} from 'react-native';
 
 function App() {
-  const [data, setData] = useState(undefined);
+  const [data, setData] = useState([]);
   const getAPIData = async () => {
-    //api call
     const url = 'https://jsonplaceholder.typicode.com/posts';
     let result = await fetch(url);
     result = await result.json();
@@ -15,25 +14,28 @@ function App() {
     getAPIData();
   }, []);
   return (
-    <ScrollView>
-      <Text style={{fontSize: 40}}>API Call</Text>
-      {data.length
-        ? data.map(item => (
+    <View>
+      <Text style={{fontSize: 40}}>Flatlist with API Call</Text>
+      {data.length ? (
+        <FlatList
+          data={data}
+          renderItem={({item}) => (
             <View
               style={{
                 padding: 10,
-                borderBottomColor: '#ccc',
+                borderBottomColor: 'orange',
                 borderBottomWidth: 1,
               }}>
-              <Text style={{fontSize: 20, backgroundColor: '#ddd'}}>
+              <Text style={{fontSize: 20, backgroundColor: 'aqua'}}>
                 id : {item.id}
               </Text>
-              <Text style={{fontSize: 20}}>{item.title}</Text>
-              <Text style={{fontSize: 20}}>{item.body}</Text>
+              <Text style={{fontSize: 18}}>{item.title}</Text>
+              <Text style={{fontSize: 18}}>{item.body}</Text>
             </View>
-          ))
-        : null}
-    </ScrollView>
+          )}
+        />
+      ) : null}
+    </View>
   );
 }
 
