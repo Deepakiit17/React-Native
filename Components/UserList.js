e8d57ee816fba2530410a5d7ce90524c0acec04f
Deepak Meena <deepak07itmeena@gmail.com>
2024-06-10 17:26:24 +0530

    call api in redux saga

--- Components/UserList.js
+++ Components/UserList.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import {getUserList} from './redux/action';
 
 const UserList = () => {
+  const dispatch = useDispatch();
+  const userList = useSelector(state => state.reducer);
+
+  useEffect(() => {
+    dispatch(getUserList());
+  }, []);
   return (
     <View>
       <Text>User List Screen</Text>
